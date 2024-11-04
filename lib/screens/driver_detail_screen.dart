99cdc61671610e777505d842202803596e35638f
Rishi Chhabra <rishi.chhabra@sparkspite.com>
2024-11-04 14:08:39 -0500

    fixed minor issue

--- lib/screens/driver_detail_screen.dart
+++ lib/screens/driver_detail_screen.dart
@@ -1,119 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:pit_stop/data/stamdings_model.dart';
+import 'package:pit_stop/utilities.dart';
+import '../data/stamdings_model.dart';
 
-class DriverDetailScreen extends StatefulWidget {
+class DriverDetailScreen extends StatelessWidget {
   const DriverDetailScreen({super.key, required this.driver});
 
   final Driver driver;
 
   @override
-  State<DriverDetailScreen> createState() => _DriverDetailScreenState();
-}
-
-class _DriverDetailScreenState extends State<DriverDetailScreen> {
-  @override
   Widget build(BuildContext context) {
+    const textStyle = TextStyle(color: Colors.white);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Driver Details ${widget.driver.name}'),
+        title: Text('Driver Details ${driver.name}'),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: const RadialGradient(
-          colors: [Colors.black87, Colors.black],
-          radius: 0.65,
-        )),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              child: Text(
-                widget.driver.name,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
+      body: SizedBox.expand(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: RadialGradient(
+              colors: [Colors.black87, Colors.black],
+              radius: 0.65,
             ),
-            Container(
-              child: Text(
-                widget.driver.number,
-                style: TextStyle(color: Colors.white, fontSize: 40),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.points,
-                style: TextStyle(color: Colors.white, fontSize: 30),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.wins,
-                style: TextStyle(color: Colors.white, fontSize: 15),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.podiums,
-                style: TextStyle(color: Colors.white, fontSize: 40),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.poles,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.dnfs,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.drivercode,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.team,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.firstentry,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.firstwin,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.wc,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.dob,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            Container(
-              child: Text(
-                widget.driver.country,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            Container(
-              child: Text(widget.driver.details),
-            ),
-          ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(driver.name, style: textStyle.copyWith(fontSize: 20)),
+              Text(driver.number, style: textStyle.copyWith(fontSize: 40)),
+              Text(driver.points, style: textStyle.copyWith(fontSize: 30)),
+              Text(driver.wins, style: textStyle.copyWith(fontSize: 15)),
+              Text(driver.podiums, style: textStyle.copyWith(fontSize: 40)),
+              Text(driver.poles, style: textStyle.copyWith(fontSize: 20)),
+              Text(driver.dnfs, style: textStyle.copyWith(fontSize: 20)),
+              Text(driver.drivercode, style: textStyle.copyWith(fontSize: 20)),
+              Text(driver.team, style: textStyle.copyWith(fontSize: 20)),
+              Text(driver.firstentry, style: textStyle.copyWith(fontSize: 20)),
+              Text(driver.firstwin, style: textStyle.copyWith(fontSize: 20)),
+              Text(driver.wc, style: textStyle.copyWith(fontSize: 20)),
+              Text(driver.dob, style: textStyle.copyWith(fontSize: 20)),
+              Text(driver.country, style: textStyle.copyWith(fontSize: 20)),
+              Text(driver.details, style: textStyle),
+            ],
+          ),
         ),
       ),
     );
